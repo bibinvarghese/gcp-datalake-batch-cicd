264649e3252504f418de2f1d61bad9f11fbdcc70
bibinvarghese <bibinvargh@gmail.com>
2020-09-12 17:31:04 -0700

    Update main.tf
    Data Lake CI/CD Testing 1 by bibin on 9/12/2020 updated

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Data Lake CI/CD Testing 1 by bibin on 9/12/2020
+# Data Lake CI/CD Testing 1 by bibin on 9/12/2020 updated 2
 terraform {
   required_version = ">= 0.12.0"
 }
